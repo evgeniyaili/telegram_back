5697118707d455f9a9620f80022479b509821854
evgeniyaili <ilinaevgeniya@mail.ru>
2023-04-21 20:45:24 +0300

    1

--- sequelize.js
+++ sequelize.js
@@ -2,9 +2,9 @@ import { Sequelize } from 'sequelize'
 
 
 export default new Sequelize(
-    'postgres', 
-    'postgres', 
-    'qaswed', 
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
@@ -17,20 +17,3 @@ export default new Sequelize(
         timezone: 'Europe/Moscow',
     }
 )
-
-// export default new Sequelize(
-//     process.env.DB_NAME, 
-//     process.env.DB_USER, 
-//     process.env.DB_PASS, 
-//     {
-//         dialect: 'postgres',
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         define: {
-//             // в базе данных поля будут created_at и updated_at
-//             underscored: true
-//         },
-//         logging: false,
-//         timezone: 'Europe/Moscow',
-//     }
-// )
