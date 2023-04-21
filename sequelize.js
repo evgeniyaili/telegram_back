cf07812ec04c4042b0c0d25ada20327769886b24
evgeniyaili <ilinaevgeniya@mail.ru>
2023-04-21 21:18:02 +0300

    1

--- sequelize.js
+++ sequelize.js
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize'
-
+require('dotenv').config()
 
 export default new Sequelize(
-    'postgres://postgres:postgres@192.168.0.2:5432/postgres',
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PASS, 
     {
         dialect: 'postgres',
-       
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
         define: {
             // в базе данных поля будут created_at и updated_at
             underscored: true
