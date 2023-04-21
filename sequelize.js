5b6bd94fa19047abdc0c8d4ff5dda638e10a7770
evgeniyaili <ilinaevgeniya@mail.ru>
2023-04-21 21:13:58 +0300

    1

--- sequelize.js
+++ sequelize.js
@@ -5,8 +5,7 @@ export default new Sequelize(
     'postgres://postgres:postgres@192.168.0.2:5432/postgres',
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+       
         define: {
             // в базе данных поля будут created_at и updated_at
             underscored: true
