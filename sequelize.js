7db5554dfcbfa07b65f2e026ac0cf22df2ae7149
evgeniyaili <ilinaevgeniya@mail.ru>
2023-04-21 23:29:37 +0300

    1

--- sequelize.js
+++ sequelize.js
@@ -2,9 +2,9 @@ import { Sequelize } from 'sequelize'
 import config from 'dotenv/config'
 
 export default new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASS, 
+    "test", 
+    "test", 
+    "qaswed", 
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
